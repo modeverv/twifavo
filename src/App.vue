fc0692c8ef4ef0db247d48bf014a764a8f69a9c3
nora inu <modeverv@gmail.com>
2018-11-07 15:16:33 +0900

    add loading

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,9 @@
         {{ option.key }}
       </option>
     </select>
-
+    <div v-if="favos.length == 0">
+    	 <div class="loader">Loading...</div>
+    </div>
     <div class="card" style="margin-bottom:1em;" v-for='(favo,i) in favos'>
       <div class="form-row align-items-center card-body">
         <div class="col-auto" style="max-width:7em;">
@@ -117,7 +119,6 @@ export default {
         });
     },
     changeKey: function(event) {
-      //alert(8)
       this.se = event.target.value;
     },
     changeTag: function(event, i) {
@@ -163,5 +164,64 @@ export default {
   /*text-align: center;*/
   color: #2c3e50;
   margin-top: 30px;
+}
+.loader,
+.loader:before,
+.loader:after {
+  background: #aaa;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loader {
+  color: #aaa;
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loader:before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 1.5em;
+}
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
 }
 </style>
